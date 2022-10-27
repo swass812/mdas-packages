259b831fb4649e4c7c5b437f849dfeaa03cecb8c
morgan-iverson <miverson@vmware.com>
2022-10-27 16:46:34 -0700

    Test environment varibale set

--- packages/gemfire/setup.d/01-setup-user.sh
+++ packages/gemfire/setup.d/01-setup-user.sh
@@ -5,15 +5,16 @@
 set -x
 set -eo pipefail
 
-echo $PROJECT_DIR
-echo $(cd "$(dirname $BASH_SOURCE)/.."; pwd)
+PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)
+
+echo $VMWARE_GEMFIRE_TARGET
+
 cd $PROJECT_DIR/gemfire
 
 # INSTALL & UNPACK
 # curl -o $VMWARE_GEMFIRE_TARGET.tgz https://spring-modern-data-architecture-files.s3.us-west-1.amazonaws.com/vmware-gemfire-9.15.0.tgz
 tar -xvf $VMWARE_GEMFIRE_TARGET.tgz
 
-# cd $PROJECT_DIR
 
 # INSTALL & UNPACK GEMFIRE FOR REDIS
 # curl -o $GEMFIRE_FOR_REDIS_TARGET.tgz https://spring-modern-data-architecture-files.s3.us-west-1.amazonaws.com/gemfire-for-redis-apps-1.0.1.tgz
