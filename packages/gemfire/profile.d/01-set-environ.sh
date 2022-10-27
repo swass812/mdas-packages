d082db5796a3896016504d6335b08ec21c502026
morgan-iverson <miverson@vmware.com>
2022-10-27 16:59:17 -0700

    Test environmen 2

--- packages/gemfire/profile.d/01-set-environ.sh
+++ packages/gemfire/profile.d/01-set-environ.sh
@@ -1,9 +1,6 @@
+export PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)/gemfire
 
-# Environment Set Up (Create Environment Variables)
-
-PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)/gemfire
-
-GEMFIRE_FOR_REDIS_TARGET=$PROJECT_DIR/gemfire-for-redis-apps-1.0.1
-VMWARE_GEMFIRE_TARGET=$PROJECT_DIR/vmware-gemfire-9.15.0
+export GEMFIRE_FOR_REDIS_TARGET=$PROJECT_DIR/gemfire-for-redis-apps-1.0.1
+export VMWARE_GEMFIRE_TARGET=$PROJECT_DIR/vmware-gemfire-9.15.0
 
 PATH=$VMWARE_GEMFIRE_TARGET/bin:$PATH
