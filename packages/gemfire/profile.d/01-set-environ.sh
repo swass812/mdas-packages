db2b422361ffc8f5b7f3257179ba094a346a1fa5
morgan-iverson <miverson@vmware.com>
2022-10-27 15:45:18 -0700

    Update working directory for gemfire/profile.d

--- packages/gemfire/profile.d/01-set-environ.sh
+++ packages/gemfire/profile.d/01-set-environ.sh
@@ -5,7 +5,9 @@
 set -x
 set -eo pipefail
 
-export PROJECT_DIR=$HOME
+
+export PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)
+# $HOME
 
 export GEMFIRE_FOR_REDIS_TARGET=$HOME/gemfire/gemfire-for-redis-apps-1.0.1
 export VMWARE_GEMFIRE_TARGET=$HOME/gemfire/vmware-gemfire-9.15.0
