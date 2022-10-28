4f8ae44052b9292e4db14630d5a578422777ce02
morgan-iverson <miverson@vmware.com>
2022-10-28 14:54:16 -0700

    Add export to postgres profil env

--- rabbitmq-3.10/setup.d/setup.sh
+++ rabbitmq-3.10/setup.d/setup.sh
@@ -6,8 +6,23 @@ set -eo pipefail
 
 PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd) # /opt/packages/postgres-12/
 
-kubectl apply -f $PROJECT_DIR/resources/
-kubectl rollout status deployment/postgres
+kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
+kubectl apply -f https://raw.githubusercontent.com/rabbitmq/cluster-operator/main/docs/examples/hello-world/rabbitmq.yaml
+# apiVersion: rabbitmq.com/v1beta1
+# kind: RabbitmqCluster
+# metadata:
+#     name: hello-world
 
 
 # TEST: k get all
+
+#https://www.rabbitmq.com/kubernetes/operator/quickstart-operator.html
+
+# username="$(kubectl get secret hello-world-default-user -o jsonpath='{.data.username}' | base64 --decode)"
+# echo "username: $username"
+# password="$(kubectl get secret hello-world-default-user -o jsonpath='{.data.password}' | base64 --decode)"
+# echo "password: $password"
+
+# can i make a secret?
+
+# https://hub.docker.com/_/rabbitmq
