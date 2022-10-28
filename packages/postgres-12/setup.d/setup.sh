9e050f6d1318672db03d041cc7e0d4d30b03ff94
morgan-iverson <miverson@vmware.com>
2022-10-28 01:59:09 -0700

    Test postgres profil.d

--- packages/postgres-12/setup.d/setup.sh
+++ packages/postgres-12/setup.d/setup.sh
@@ -5,8 +5,8 @@ set -x
 set -eo pipefail
 
 PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd) # /opt/packages/postgres-12/
-# mkdir -p $PROJECT_DIR/dir-in-workshop
 
-# echo $PROJECT_DIR
 kubectl apply -f $PROJECT_DIR/resources/
 kubectl rollout status deployment/postgres
+
+# TEST: k get all
