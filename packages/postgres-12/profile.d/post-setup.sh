41643e753c89457a6c0a76ef71677e88abc913c4
morgan-iverson <miverson@vmware.com>
2022-10-27 23:04:48 -0700

    Test

--- packages/postgres-12/profile.d/post-setup.sh
+++ packages/postgres-12/profile.d/post-setup.sh
@@ -1,4 +1,4 @@
-POSTGRES_POD_NAME=$(kubectl get pod -l app=postgres -o jsonpath="{.items[0].metadata.name}")
+# POSTGRES_POD_NAME=$(kubectl get pod -l app=postgres -o jsonpath="{.items[0].metadata.name}")
 alias psql='kubectl exec -it $POSTGRES_POD_NAME -- psql'
 
 # PATH=test:$PATH
