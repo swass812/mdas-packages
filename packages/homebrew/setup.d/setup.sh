93e5bbfebff7c4ba4e5785558a1b2de7380316bf
morgan-iverson <miverson@vmware.com>
2022-10-31 20:02:50 -0700

    Remove Unzip

--- packages/homebrew/setup.d/setup.sh
+++ packages/homebrew/setup.d/setup.sh
@@ -8,10 +8,10 @@ PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)"; pwd)/homebrew
 HOMEBREW_TARGET=$PROJECT_DIR/homebrew
 
 cd $PROJECT_DIR
-tar xz --strip 1 HOMEBREW_TARGET.tgz
-eval "$(homebrew/bin/brew shellenv)" # ??
-brew update --force --quiet
-chmod -R go-w "$(brew --prefix)/share/zsh"
+# tar xz --strip 1 HOMEBREW_TARGET.tgz
+# eval "$(homebrew/bin/brew shellenv)" # ??
+# brew update --force --quiet
+# chmod -R go-w "$(brew --prefix)/share/zsh"
 
 # mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 # eval "$(homebrew/bin/brew shellenv)"
