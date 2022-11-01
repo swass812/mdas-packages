cea259676daaa6948fb7783b699bcc81d062ede7
morgan-iverson <miverson@vmware.com>
2022-10-31 19:54:14 -0700

    Fix naming errors

--- packages/homebrew/build.sh
+++ packages/homebrew/build.sh
@@ -6,9 +6,9 @@ set -eo pipefail
 
 PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)"; pwd)
 
-HOMEBREW_TARGET=$PROJECT_DIR/hombrew/Hombrew
+HOMEBREW_TARGET=$PROJECT_DIR/homebrew/Homebrew
 
-mkdir -p $PROJECT_DIR/hombrew
+mkdir -p $PROJECT_DIR/homebrew
 
 # INSTALL & UNPACK
 curl -o $HOMEBREW_TARGET.tgz https://github.com/Homebrew/brew/tarball/master
